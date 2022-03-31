70ac7eef1223bf2176dc8ed5dd779d76aeddad45
venous666 <99412095+venous666@users.noreply.github.com>
2022-03-31 12:52:41 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+nota = Math.floor (Math.random() * 10) + 1
+console.log(nota)
+
+if (nota >= 7){  
+    console.log("Aprovado")
+} else if (nota >= 3){
+    console.log("Recuperaçao")
+}
+ else {
+    console.log("Reprovado")
+}
