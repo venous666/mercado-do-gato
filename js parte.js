bbc16eba5c952db1acc933176fbab617e0593a6f
venous666 <99412095+venous666@users.noreply.github.com>
2022-03-31 12:53:27 -0300

    Create js parte.js

---
+++ js parte.js
@@ -0,0 +1,20 @@
+
+let produtos = document.querySelector( '#produtos' )
+let botao = document.querySelector( '#comprar')
+
+botao.addEventListener( 'click', comprar)
+function comprar (){
+    compra = "produto comprado\n"
+    if (produtos.value == "whiskas"){
+        compra += "produto: " + produtos.value + " , valor: R$ 15,00"
+    }
+   else if (produtos.value == "friskies"){
+        compra += "produto: " + produtos.value + " , valor: R$ 13,00"
+    }
+  else  if (produtos.value == "sopa salgada"){
+        compra += "produto: " + produtos.value + " , valor: R$ 11,00"
+    }
+   else  if (produtos.value == "sopinha boa"){
+        compra += "produto: " + produtos.value + " , valor: R$ 20,00"
+    }
+    alert (compra)    
